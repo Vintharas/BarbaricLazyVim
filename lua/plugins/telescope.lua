a27423fb0956225f648379b6e99373b3287d8e0e
Jaime Gonzalez <jaime.gg83@gmail.com>
2023-02-19 10:45:54 +0100

    Updated telescope config to include some custom mappings to traverse selection list

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -28,14 +28,27 @@ return {
       },
     },
     -- change some options
-    -- opts = {
-    --   defaults = {
-    --     layout_strategy = "horizontal",
-    --     layout_config = { prompt_position = "top" },
-    --     sorting_strategy = "ascending",
-    --     winblend = 0,
-    --   },
-    -- },
+    opts = {
+      --   defaults = {
+      --     layout_strategy = "horizontal",
+      --     layout_config = { prompt_position = "top" },
+      --     sorting_strategy = "ascending",
+      --     winblend = 0,
+      --   },
+      -- add some mappings
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = function(...)
+              return require("telescope.actions").move_selection_next(...)
+            end,
+            ["<C-k>"] = function(...)
+              return require("telescope.actions").move_selection_previous(...)
+            end,
+          },
+        },
+      },
+    },
   },
   -- add telescope-fzf-native
   -- {
