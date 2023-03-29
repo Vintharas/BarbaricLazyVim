c0644f13872ae2180539981433d1281d15fb0fef
Jaime Gonzalez <jaime.gg83@gmail.com>
2023-03-29 20:21:15 +0200

    Started adding configs for mini

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,20 @@
+-- Configs for mini.nvim
+return {
+
+  -- Update mini.surround mappings to use gs instead of gz
+  -- Using gs conflicts with leap.
+  -- {
+  --   "echasnovski/mini.surround",
+  --   opts = {
+  --     mappings = {
+  --       add = "gsa",
+  --       delete = "gsd",
+  --       find = "gsf",
+  --       find_left = "gsF",
+  --       highlight = "gsh",
+  --       replace = "gsr",
+  --       update_n_lines = "gsn",
+  --     },
+  --   },
+  -- },
+}
