335aa98ab0c5dc83592a761efc2c2f01af1846d2
Jaime Gonzalez Garcia <jaime.gg83@gmail.com>
2024-08-17 21:33:31 +0200

    Play with annotations

--- dev/surround/lua/surround.lua
+++ dev/surround/lua/surround.lua
@@ -1,13 +1,19 @@
-
 local M = {}
 
 -- Test whether the plugin is actually loaded
 -- print('hello world')
 
 -- Register an ex-command
-vim.api.nvim_create_user_command('HelloWorld', 'echo "Hello world!"', {})
+vim.api.nvim_create_user_command("HelloWorld", 'echo "Hello world!"', {})
 
 -- Setup plugin
 M.setup = function() end
+
+---@alias MyCustomType integer
+
+---Calculate a value using [my custom type](lua://MyCustomType)
+local function calculate(x)
+  print(x)
+end
 
 return M
