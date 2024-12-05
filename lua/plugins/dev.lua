dd023ca127ab66da7e7a96024eb3307341c33f5c
Jaime Gonzalez Garcia <jaime.gg83@gmail.com>
2024-12-05 11:55:05 +0100

    Added some dev plugins

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,10 +1,26 @@
 -- Plugins under development
 return {
   {
-  name = "vintharas/surround",
-  dir = "~/.config/nvim/dev/surround",
-  dev = true,
-  lazy = false,
-  opts = {},
+    name = "vintharas/surround",
+    dir = "~/.config/nvim/dev/surround",
+    dev = true,
+    lazy = false,
+    opts = {},
+  },
+  {
+    name = "vintharas/llm",
+    dir = "~/GitHub/llm.nvim",
+    dev = true,
+    lazy = false,
+    opts = {
+      -- debug_level = "DEBUG",
+    },
+  },
+  {
+    name = "vintharas/goog-terms.nvim",
+    dir = "~/GitHub/goog-terms.nvim",
+    dev = true,
+    lazy = false,
+    opts = {},
   },
 }
