9cb9519c52e727095f2136cddb765c3b19b1a9d4
Jaime Gonzalez Garcia <jaime.gg83@gmail.com>
2024-12-28 20:44:32 +0100

    Add cyberdream colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,4 +1,13 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
   { "catppuccin/nvim" },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+  { "LazyVim/LazyVim", opts = { colorscheme = "cyberdream" } },
 }
