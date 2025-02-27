f46a60a47606a76601f2e0098eced2d7887521d4
Jaime Gonzalez Garcia <jaime.gg83@gmail.com>
2025-02-27 17:20:18 +0100

    Update plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,18 +9,18 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "catppuccin"}},
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "catppuccin" }},
     -- Extras
-    { import = "lazyvim.plugins.extras.ai.copilot"},
-    { import = "lazyvim.plugins.extras.coding.mini-surround"},
-    { import = "lazyvim.plugins.extras.coding.yanky"},
-    { import = "lazyvim.plugins.extras.formatting.prettier"},
-    { import = "lazyvim.plugins.extras.lang.go"},
-    { import = "lazyvim.plugins.extras.lang.java"},
-    { import = "lazyvim.plugins.extras.lang.json"},
-    { import = "lazyvim.plugins.extras.lang.markdown"},
-    { import = "lazyvim.plugins.extras.lang.python"},
-    { import = "lazyvim.plugins.extras.lang.typescript"},
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- import/override with your plugins
     { import = "plugins" },
   },
